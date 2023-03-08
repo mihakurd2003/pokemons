f3e8b643fdf9b682f86bc8f6c3c84317cdddc9a1
S1im_Sh4dy <mihakurd@gmail.com>
2023-03-08 13:10:22 +0300

    Added 'verbose name' in models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,28 +2,35 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=100)
-    title_en = models.CharField(max_length=100, default='title en')
-    title_jp = models.CharField(max_length=100, default='title jp')
-    image = models.ImageField(upload_to='pokemon', null=True, blank=True)
-    description = models.TextField()
-    evolution_from = models.ForeignKey('self', on_delete=models.CASCADE, related_name='evolution_to', null=True, blank=True)
+    """Покемон"""
+    title_ru = models.CharField('Название(рус)', max_length=100)
+    title_en = models.CharField('Название(англ)', max_length=100)
+    title_jp = models.CharField('Название(яп)', max_length=100)
+    image = models.ImageField('Картинка', upload_to='pokemon', null=True, blank=True)
+    description = models.TextField('Описание')
+    evolution_from = models.ForeignKey(
+        verbose_name='Эволюция(из кого)',
+        to='self', on_delete=models.CASCADE,
+        related_name='evolution_to',
+        null=True, blank=True,
+    )
 
     def __str__(self):
         return self.title_ru
 
 
 class PokemonEntity(models.Model):
-    lat = models.FloatField()
-    lon = models.FloatField()
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    """Объект покемона"""
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    pokemon = models.ForeignKey(verbose_name='Покемон', to=Pokemon, on_delete=models.CASCADE)
+    appeared_at = models.DateTimeField('Появился')
+    disappeared_at = models.DateTimeField('Исчез')
+    level = models.IntegerField('Уровень')
+    health = models.IntegerField('Здоровье')
+    strength = models.IntegerField('Сила')
+    defence = models.IntegerField('Защита')
+    stamina = models.IntegerField('Выносливость')
 
     def __str__(self):
         return f'Pokemon entity of {self.pokemon.title_ru}'
